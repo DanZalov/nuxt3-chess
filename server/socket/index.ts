036c4b8ad6ec287546d0cb0849f2ee3f3be23c11
DanZalov <danzalov@gmail.com>
2023-04-21 05:11:19 +0400

    Saving move history. Clear if everyone left

--- server/socket/index.ts
+++ server/socket/index.ts
@@ -1,15 +1,28 @@
 import { defineIOHandler } from '../../modules/socket'
 
 export default defineIOHandler((io) => {
+  let moveHistory: string[] = []
   io.on('connection', (socket) => {
     console.log('Connected ', socket.id)
+    moveHistory.forEach((move) => {
+      socket.emit('move', move)
+    })
+
     socket.on('message', (message) => {
       console.log(`Server heard from ${socket.id}: `, message)
       socket.emit('message', message)
     })
+
     socket.on('move', (message) => {
+      moveHistory.push(message)
       console.log(`Server heard from ${socket.id}: `, message)
       socket.broadcast.emit('move', message)
+    })
+    socket.on('disconnect', () => {
+      console.log(`${socket.id} disconnected`)
+      if (!io.of('/').sockets.size) {
+        moveHistory = []
+      }
     })
   })
 })
