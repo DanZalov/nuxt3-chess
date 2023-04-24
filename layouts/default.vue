3cc76511d0f192879002d619e27a1aa04d469812
DanZalov <danzalov@gmail.com>
2023-04-24 20:15:51 +0400

    Updated home page, added layout

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app-layout">
+    <Head>
+      <Title>Chess player</Title>
+      <Meta name="description" content="online chess player" />
+      <Meta
+        name="keywords"
+        content="chess, nuxt, vue, vuetify, typescript, websocket"
+      />
+      <Meta name="author" content="Danil Zalialutdinov" />
+    </Head>
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<style>
+body {
+  background-color: #f5deb3;
+}
+</style>
--- pages/Home.vue
+++ pages/Home.vue
@@ -1,13 +1,14 @@
 <template>
   <v-container class="d-flex flex-row justify-center align-center">
-    <img src="/KnightLogo-compressed.jpg" alt="logo" height="400" />
+    <!-- <img src="/KnightLogo-compressed.jpg" alt="logo" height="400" /> -->
     <v-card
-      height="380"
-      class="d-flex flex-column justify-space-between align-center bg-green-lighten-5"
+      height="540"
+      width="421"
+      class="d-flex flex-column justify-space-between align-center knight-image"
     >
-      <v-card-title class="text-h1 pa-10">Chess</v-card-title>
+      <v-card-title class="text-h1 pa-10 text-green">Chess</v-card-title>
       <div class="d-flex flex-column align-start">
-        <v-btn size="x-large" class="w-100 my-2 bg-green">Play</v-btn>
+        <v-btn size="x-large" class="w-100 my-7 bg-green">Play</v-btn>
         <v-btn size="x-large" class="w-100 bg-green">
           <NuxtLink to="/board">Use a board</NuxtLink>
         </v-btn>
@@ -20,5 +21,10 @@
 a {
   text-decoration: none;
   color: white;
+}
+.knight-image {
+  background-image: url('/KnightLogo-450compressed.jpg');
+  background-repeat: no-repeat;
+  background-position: bottom;
 }
 </style>
